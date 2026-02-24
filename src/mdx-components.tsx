3c9b63f85ca6c6dd13bdea2e1aa6a292fbf8d2a2
Shamim <hossains159@gmail.com>
2026-02-24 16:29:42 +0600

    feat: setup index and setup pages

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,5 @@
+import * as FilesComponents from "fumadocs-ui/components/files";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
@@ -6,6 +8,8 @@ import { Preview } from "@/components/preview";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
+    ...FilesComponents,
     ...components,
     Preview,
   };
