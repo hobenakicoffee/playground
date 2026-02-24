15ffafca1cdcc8bc569294e897e5013d1503844b
Shamim <hossains159@gmail.com>
2026-02-24 18:53:59 +0600

    chore: setup mermaid diagram support

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,7 @@ import * as FilesComponents from "fumadocs-ui/components/files";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import { Mermaid } from "@/components/mdx/mermaid";
 
 import { Preview } from "@/components/preview";
 
@@ -10,7 +11,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...defaultMdxComponents,
     ...TabsComponents,
     ...FilesComponents,
-    ...components,
+    Mermaid,
     Preview,
+    ...components,
   };
 }
